a7d2a00d30265cc5edfdbb215f47c32745dd604b
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-09 23:54:44 +0000

    @mmmbacon Pair program. Diagnol array Partially Implemented

--- wordsearch.js
+++ wordsearch.js
@@ -27,45 +27,51 @@ const wordSearch = (letters, word) => {
         }
     }
 
-   // searchDiagnol(letters,word);
+    searchDiagnol(letters,word);
 
     return result;
 }
 
-// const searchDiagnol = (letters, word) => {
-//     let letter = null;
-//     const arr = [];
-//     for (let i = 0; i < 5; i++) {
-//         let x = i;        
-//         let y = 0;
+const searchDiagnol = (letters, word) => {
+    let letter = null;
+    let result = false;
 
-//         do {
-//             if (letters[x][y]) {
-//                 arr.push(letters[x][y]);
-//             }
+    for (let i = 0; i < letters.length; i++) {
+        const arr = [];
+        let x = 0;        
+        let y = i;
 
-//             x++;
-//             y++;
-//             console.log(letters[x][y]);
-//         } while (letters[x][y]);
-//         const diagString = arr.join('');
-//         console.log(diagString);
-//     }
+        do {
+            if (letters[x][y]) {
+                arr.push(letters[x][y]);
+            }
+            // console.log(letters[x][y]);
+            x++;
+            y++;
+        } while (letters[x][y]);
+        const diagString = arr.join('');
+        if(diagString.includes(word)) {
+            console.log('Arr me mateys there she blows');
+            result = true;
+        }
+        // console.log(diagString);
+        // console.log('end of diagnol');
+    }
 
-    
-// };
+    return result;
+};
 
-// wordSearch([
-//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-// ], 'VEEP')
+wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'FURL')
 
 
 module.exports = wordSearch;
